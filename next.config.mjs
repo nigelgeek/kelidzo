7eb1e9c3049d6bb2de32f67eec367ea5d20f8a2a
nigelgeek <nigelhenaku@gmail.com>
2026-09-03 15:33:54 +0000

    dist dir v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Output the production build to a `dist` directory for Vercel.
-  distDir: 'dist',
 }
 
 export default nextConfig
